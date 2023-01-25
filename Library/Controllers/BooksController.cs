a846838acc9c547630c3a930ad430a103e782c35
OLGON92 <ogon318@gmail.com>
2023-01-24 21:19:13 -0800

    be mostly finished

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -11,22 +11,119 @@ using System.Security.Claims;
 
 namespace Library.Controllers
 {
+  [Authorize]
   public class BooksController : Controller
-  {
+  { 
     private readonly LibraryContext _db;
-    public BooksController(LibraryContext db)
+    private readonly UserManager<ApplicationUser> _userManager;
+    public BooksController(UserManager<ApplicationUser> userManager, LibraryContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
-    public ActionResult Index()
+    public async Task<ActionResult> Index()
     {
-      return View(_db.Books.ToList());
+      string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+      List<Book> userBooks = _db.Books
+                          .Where(entry => entry.User.Id == currentUser.Id)
+                          .ToList();
+      return View(userBooks);
     }
 
     public ActionResult Create()
-    {
+    { 
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
       return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Book book, int AuthorId)
+    {
+      if (!ModelState.IsValid)
+      {
+        ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
+        return View(book);
+      }
+      else
+      {
+        string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+        book.User = currentUser;
+        _db.Books.Add(book);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+    public ActionResult Details(int id)
+    {
+      Book thisBook = _db.Books
+        .Include(book => book.JoinEntities)
+        .ThenInclude(join => join.Author)
+        .FirstOrDefault(book => book.BookId == id);
+        return View(thisBook);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Book thisBook =_db.Books.FirstOrDefault(book => book.BookId == id);
+      ViewBag.AuthorId = new SelectList(_db.AuthorBooks, "AuthorsId", "AuthorName");
+      return View(thisBook);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Book book)
+    {
+      _db.Books.Update(book);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Book thisBook =_db.Books.FirstOrDefault(book => book.BookId == id);
+      return View(thisBook);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Book thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      _db.Books.Remove(thisBook);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddAuthor(int id)
+    {
+      Book thisBook = _db.Books.FirstOrDefault(books => books.BookId == id);
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
+      return View(thisBook);
+    }
+
+    [HttpPost]
+    public ActionResult AddAuthor(Book book, int authorId)
+    {
+      #nullable enable
+      AuthorBook? joinEntity = _db.AuthorBooks.FirstOrDefault(join => (join.AuthorId == authorId && join.BookId == book.BookId));
+      #nullable disable
+      if (joinEntity == null & authorId != 0)
+      {
+        _db.AuthorBooks.Add(new AuthorBook() { AuthorId = authorId, BookId = book.BookId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = book.BookId});
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      AuthorBook joinEntry = _db.AuthorBooks.FirstOrDefault(entry => entry.AuthorBookId == joinId);
+      _db.AuthorBooks.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
